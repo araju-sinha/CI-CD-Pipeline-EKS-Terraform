4f0647fc663472f3c2e6afbe2f740d253c5a20bb
Aarju Sinha <49508889+araju-sinha@users.noreply.github.com>
2024-07-19 23:25:18 +0530

    Update and rename backends.tf to main.tf

---
+++ infra/terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "eks-rds-state-bucket/state"
+#    key            = "terraform/state/terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"  # Optional but recommended for state locking
+  }
+}
+
+
+#Container Registry
+resource "aws_ecr_repository" "flask_app_ecr" {
+  name = "flask_app_ecr"
+
+  tags = {
+    Name = "flask_app_ecr"
+  }
+}
+
+
