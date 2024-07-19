e1825a0a7481853790fa78949ef3fae9d5a02d90
Aarju Sinha <49508889+araju-sinha@users.noreply.github.com>
2024-07-19 22:51:54 +0530

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -11,12 +11,11 @@ terraform {
 resource "aws_ecr_repository" "flask_app_ecr" {
   name = "flask_app_ecr"
 
-  image_tag_mutability = "IMMUTABLE"
   tags = {
     Name = "flask_app_ecr"
   }
 }
 
 output "ecr_repository_url" {
-  value = aws_ecr_repository.example.repository_url
+  value       = aws_ecr_repository.flask_app_ecr.repository_url
 }
