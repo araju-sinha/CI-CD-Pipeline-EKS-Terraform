708da7b1f7d0f4258e8491bf644a0cf6e1912a8d
Aarju Sinha <49508889+araju-sinha@users.noreply.github.com>
2024-07-19 22:45:52 +0530

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,11 +1,15 @@
-provider "aws" {
-  region = "us-west-2"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
-
 provider "aws" {
   region = "us-west-2"
   access_key = ${{ secrets.AWS_ACCESS_KEY }}
-  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+  secret_key = ${{ secrets.AWS_SECRET_KEY }}
 }
 
 #Container Registry
