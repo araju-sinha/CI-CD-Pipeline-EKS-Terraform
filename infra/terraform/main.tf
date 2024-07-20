e5f42d8e07106d044392fd544f8d1077deddf7d1
Aarju Sinha <49508889+araju-sinha@users.noreply.github.com>
2024-07-20 12:30:16 +0530

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,9 +1,13 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+  backend "s3" {
+    bucket         = "eks-rds-state-bucket"
+    key            = "ecr-rds/statefile/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-lock-table"
   }
 }
 
