5c90b272431f38a25893a1bcbbd7da640daceb56
Aarju Sinha <49508889+araju-sinha@users.noreply.github.com>
2024-07-26 17:17:09 +0530

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -200,7 +200,7 @@ resource "aws_iam_role_policy_attachment" "eks_node_policy" {
 
 # Create an EKS node group
 resource "aws_eks_node_group" "node-group-01" {
-  cluster_name    = aws_eks_cluster.example.name
+  cluster_name    = aws_eks_cluster.eks-cluster-01.name
   node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
 
